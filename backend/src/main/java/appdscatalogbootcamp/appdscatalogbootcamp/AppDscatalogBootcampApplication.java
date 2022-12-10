a16991c88721a10fbfd57a2fb66114cc200d195c
Alan <socyware@gmail.com>
2022-12-10 14:45:43 -0300

    first commit

---
+++ backend/src/main/java/appdscatalogbootcamp/appdscatalogbootcamp/AppDscatalogBootcampApplication.java
@@ -0,0 +1,13 @@
+package appdscatalogbootcamp.appdscatalogbootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDscatalogBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDscatalogBootcampApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/appdscatalogbootcamp/appdscatalogbootcamp/AppDscatalogBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package appdscatalogbootcamp.appdscatalogbootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDscatalogBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
