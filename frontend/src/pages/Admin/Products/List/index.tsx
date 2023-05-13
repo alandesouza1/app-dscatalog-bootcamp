a83293da10090b4402b2916355c9fd7f981251b6
Alan <socyware@gmail.com>
2023-05-13 13:15:54 -0300

    Category Badge

--- frontend/src/pages/Admin/Products/List/index.tsx
+++ frontend/src/pages/Admin/Products/List/index.tsx
@@ -1,6 +1,6 @@
-import ProductCrudCard from "components/ProductCrudCard";
-import "./styles.css";
+import ProductCrudCard from "pages/Admin/Products/ProductCrudCard";
 import { Link } from "react-router-dom";
+import "./styles.css";
 
 const List = () => {
   const product = {
@@ -16,6 +16,10 @@ const List = () => {
       {
         id: 2,
         name: "Eletrônicos",
+      },
+      {
+        id: 1,
+        name: "Computadores",
       },
     ],
   };
